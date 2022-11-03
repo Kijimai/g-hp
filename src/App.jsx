a6cf60a05b4e8e6b05524f8bd5e88414cce85813
John Deen Bucog <JDBucog@yahoo.com>
2022-11-03 01:39:37 -0700

    remove base styles, import tailwind directives, adjust prefers color scheme, button styles from vite import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
+import Navigator from "./components/Navigator"
 import Camera from "./icons/Camera"
 import Microphone from "./icons/Microphone"
 import GridDots from "./icons/GridDots"
@@ -7,34 +7,14 @@ import MagnifyingGlass from "./icons/MagnifyingGlass"
 import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDark, setIsDark] = useState(false)
   return (
     <div className="App">
+      <Navigator />
       <Camera />
       <Microphone />
       <GridDots />
       <MagnifyingGlass />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   )
 }
